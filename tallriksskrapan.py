59ef7b6fec30672b1556bfb71e8ce5e0ca19602e
Jonas Hesselryd <jonas.hesselryd@gmail.com>
2016-05-26 19:51:26 +0200

    A bit less complicated

--- tallriksskrapan.py
+++ tallriksskrapan.py
@@ -5,12 +5,8 @@ def parse_teknikparken():
     print("### TEKNIKPARKEN ###")
     answer = requests.get('http://www.restaurangteknikparken.se/index.php?option=com_content&view=article&id=46')
     root = html.fromstring(answer.text)
-    for child in root.xpath('//div[@class="screen"]/div'):
-        if child.attrib['class'] == "meny_back":
-            break
-    menu = child.getchildren()[0]
     friday_found = False
-    for child in menu.getchildren():
+    for child in root.xpath('//div[@class="screen"]/div/div/div'):
         if friday_found and child.text:
             print(child.text)
         elif child.text and "fredag" in child.text.lower():
