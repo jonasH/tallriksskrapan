9ae5965411917ccc7477d95045c30afb2137104c
johe <johe@syntronic.com>
2016-05-27 10:27:04 +0200

    Adding parse of kompassen

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # tallriksskrapan
 
 Use Python3
+
+Getting started:
+
+pip install -r requirements.txt
+
+Of course it's not this easy on windows, google the requirements if they are not cooperative.
--- tallriksskrapan.py
+++ tallriksskrapan.py
@@ -1,6 +1,19 @@
 import requests
 from lxml import html
 
+
+def parse_kompassen():
+    print("### KOMPASSEN ###")
+    answer = requests.get('http://www.restaurangkompassen.se/index.php?option=com_content&view=article&id=64&Itemid=66')
+    root = html.fromstring(answer.text)
+    friday_found = False
+    for child in root.xpath('//div[@class="screen"]/div/div/div'):
+        if friday_found and child.text:
+            print(child.text)
+        elif child.text and "fredag" in child.text.lower():
+            friday_found = True
+
+
 def parse_teknikparken():
     print("### TEKNIKPARKEN ###")
     answer = requests.get('http://www.restaurangteknikparken.se/index.php?option=com_content&view=article&id=46')
@@ -11,9 +24,11 @@ def parse_teknikparken():
             print(child.text)
         elif child.text and "fredag" in child.text.lower():
             friday_found = True
-    
+
+
 def main():
     parse_teknikparken()
+    parse_kompassen()
 
 if __name__ == '__main__':
     main()
