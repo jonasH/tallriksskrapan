d5bd1877378dffda17213517504a7cfcfe326c39
Daniel Mattsson <danm@danm-linux.gavle.syntronic.se>
2016-12-19 10:17:54 +0100

    Added PDF parser, now prints out hemlinby lunch

--- tallriksskrapan.py
+++ tallriksskrapan.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 import requests
 import urllib.request
+import io
+import warnings
+
 from lxml import html
+from pdfminer.pdfparser import PDFParser, PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.layout import LAParams, LTTextBox, LTTextLine
 
 week_number = 0
 
@@ -51,11 +58,68 @@ def parse_hemlingby():
     root = html.fromstring(answer.text)
     for child in root.xpath('//a'):
         if child.text and "meny vecka" in child.text.lower() and week_number in child.text.lower():
-            print(child.text)
             hemlingby_link='http://www.gavle.se' + child.get('href')
-            print(hemlingby_link)
             break
-    urllib.request.urlretrieve(hemlingby_link, "hemlingby.pdf")
+    textAsArray = parse_pdf(hemlingby_link)
+    fridaysFood = getFoodFromPDFArray(textAsArray)
+    print(fridaysFood)
+    
+#Takes url to pdf file and returns text split on newline into array
+def parse_pdf(pdf_url):
+    warnings.filterwarnings('ignore', category=Warning, append=True)
+
+    remote_file = urllib.request.urlopen(pdf_url).read()
+    memory_file = io.BytesIO(remote_file)
+    parser = PDFParser(memory_file)
+    doc = PDFDocument()
+    parser.set_document(doc)
+    #Warning sometimes, error in pdf?
+    doc.set_parser(parser)
+    doc.initialize('')
+    rsrcmgr = PDFResourceManager()
+    laparams = LAParams()
+    device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+    warnings.resetwarnings()
+    warnings.filterwarnings('always', category=Warning, append=True)
+
+    ret = []
+    # Process each page contained in the document.
+    for pageIdx, page in enumerate(doc.get_pages()):
+        ret.append([])
+        interpreter.process_page(page)
+        layout = device.get_result()
+        for idx, lt_obj in enumerate(layout):
+            if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
+                if len(lt_obj.get_text().strip()) > 0:
+                    ret[pageIdx].append((lt_obj.get_text().splitlines()))
+    return ret
+
+def getFoodFromPDFArray(pdfArray):
+    correctWeek = False
+    ret = ""
+    for page in pdfArray:
+        for idx, line in enumerate(page):
+            #Check if correct week
+            if ("vecka " +  week_number + ":") in line[0]:
+                correctWeek = True;
+                continue;
+            #If correct week and day is fredag
+            elif correctWeek and "fredag" in line[0].lower():
+
+                #If the line is bigger than 1 it contains the food after 'fredag'
+                if len(line) > 1:
+                    for x in range(1, len(line)):
+                        ret += line[x] + "\n"
+                #The line only contained 'fredag' so the food is in the line after 'fredag'
+                else:
+                    line = page[idx+1]
+                    for x in range(0, len(line)):
+                        ret += line[x] + "\n"
+                return ret
+
+    return "Oops something went wrong"
 
 def parse_gustafsbro():
     print("### Gustafsbro ###")
